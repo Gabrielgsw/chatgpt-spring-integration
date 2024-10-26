e9238fd1918111f6f6d2d31c6de252e352d8a5f5
Gabrielgsw <ggermanow279@gmail.com>
2024-10-25 21:53:33 -0300

    initial commit

---
+++ src/main/java/br/com/chatgpt_spring/chatgptspringintegration/ChatgptSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.chatgpt_spring.chatgptspringintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptSpringIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptSpringIntegrationApplication.class, args);
+    }
+
+}
